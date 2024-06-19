d0e58c0555513e7a4b20cceb24ab27417f44b1c6
loginovw8 <loginovw8@gmail.com>
2024-06-19 14:02:13 +0300

    Zoom integrated.

--- README.md
+++ README.md
@@ -17,3 +17,11 @@ https://www.youtube.com/watch?v=_HkGH2WN_Tg&list=PLB0Tybl0UNfYoJE7ZwsBQoDIG4YN9p
 006-dragging-points
 007-creating-segments
 008-fine-tuning
+
+## Part 03 - Pan & Zoom
+
+https://www.youtube.com/watch?v=S4HMcN2YlgU&list=PLB0Tybl0UNfZtY5IQl1aNwcoOPJNtnPEO&index=3
+
+009-zoom
+010-drag-and-pan
+011-saving-and-loading
--- js/graphEditor.js
+++ js/graphEditor.js
@@ -1,6 +1,8 @@
 class GraphEditor {
-    constructor(canvas, graph) {
-        this.canvas = canvas;
+    constructor(viewport, graph) {
+        this.viewport = viewport;
+        this.canvas = viewport.canvas;
+
         this.graph = graph;
 
         this.ctx = this.canvas.getContext("2d");
@@ -46,9 +48,9 @@ class GraphEditor {
     }
 
     #handleMouseMove(evt) {
-        this.mouse = new Point(evt.offsetX, evt.offsetY);
+        this.mouse = this.viewport.getMouse(evt);
 
-        this.hovered = getNearestPoint(this.mouse, this.graph.points, 10);
+        this.hovered = getNearestPoint(this.mouse, this.graph.points, 10 * this.viewport.zoom);
 
         if (this.dragging) {
             this.selected.x = this.mouse.x;
