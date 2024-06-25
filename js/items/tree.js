88b517f7a34892ec14781e8c834283866b2e4278
loginovw8 <loginovw8@gmail.com>
2024-06-25 18:41:32 +0300

    Styling 3d trees.

--- js/items/tree.js
+++ js/items/tree.js
@@ -5,9 +5,20 @@ class Tree {
         this.heightCoef = heightCoef;
     }
 
+    #generateLevel(point, size) {
+        const points = [];
+        const rad = size / 2;
+
+        for (let a = 0; a < Math.PI * 2; a += Math.PI / 16) {
+            const noisyRadius = rad * lerp(0.5, 1, Math.random());
+            points.push(translate(point, a, noisyRadius));
+        }
+
+        return new Polygon(points);
+    }
+
     draw(ctx, viewPoint) {
         const diff = subtract(this.center, viewPoint);
-
         const top = add(this.center, scale(diff, this.heightCoef));
 
         const levelCount = 7;
@@ -15,7 +26,10 @@ class Tree {
         for (let level = 0; level < levelCount; level++) {
             const t = level / (levelCount - 1);
             const point = lerp2D(this.center, top, t);
-            point.draw(ctx, { size: this.size, color: "green" });
+            const color = "rgb(30, " + lerp(50, 200, t) + ", 70)";
+            const size = lerp(this.size, 40, t);
+            const poly = this.#generateLevel(point, size);
+            poly.draw(ctx, { fill: color, stroke: "rgba(0, 0, 0, 0)" });
         }
 
         new Segment(this.center, top).draw(ctx);
